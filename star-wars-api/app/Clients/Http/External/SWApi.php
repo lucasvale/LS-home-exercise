e6609bc9683f46798b46d0b8c50b49cdf6448f37
Lucas Silva <lucasfaustino@movida.com.br>
2025-12-29 17:54:02 -0300

    adding cache

--- star-wars-api/app/Clients/Http/External/SWApi.php
+++ star-wars-api/app/Clients/Http/External/SWApi.php
@@ -7,14 +7,20 @@ use App\Data\People;
 use App\Data\Request\FindFilmsRequest;
 use App\Data\Request\FindPeopleRequest;
 use Illuminate\Http\Client\ConnectionException;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 
 readonly class SWApi
 {
-    private string  $base_url;
+    private string $base_url;
+    private string $base_key_cache;
+    private int $expiration_time;
+
     public function __construct()
     {
         $this->base_url = config('services.swapi.base_uri');
+        $this->base_key_cache = 'external_api_data:';
+        $this->expiration_time = 60;
     }
 
     /**
@@ -22,24 +28,31 @@ readonly class SWApi
      */
     public function getPeople(FindPeopleRequest $request): People
     {
-        $url = $this->base_url.\App\Enums\SWApi::PEOPLE->value;
+        $url = $this->base_url . \App\Enums\SWApi::PEOPLE->value;
         $query_parameters = [];
-        if (!empty($request->name)){
+        $key = $this->base_key_cache . $url;
+        if (!empty($request->name)) {
             $query_parameters['name'] = $request->name;
+            $key .= $request->name;
         }
-        $response = Http::withQueryParameters($query_parameters)->get($url);
-        $data = $response->json();
+        $data = Cache::remember(md5($key), $this->expiration_time, function () use ($url, $query_parameters) {
+            $response = Http::withQueryParameters($query_parameters)->get($url);
+            return $response->json();
+        });
         return People::fromSWApiResponse($data);
     }
 
     public function getPeopleById(string $id): People
     {
-        $url = $this->base_url.\App\Enums\SWApi::PEOPLE->value;
-        if (!empty($id)){
-            $url .= '/'.$id;
+        $url = $this->base_url . \App\Enums\SWApi::PEOPLE->value;
+        if (!empty($id)) {
+            $url .= '/' . $id;
         }
-        $response = Http::get($url);
-        $data = $response->json();
+        $key = $this->base_key_cache . $url;
+        $data = Cache::remember(md5($key), $this->expiration_time, function () use ($url) {
+            $response = Http::get($url);
+            return $response->json();
+        });
         return People::fromSWApiResponse($data);
     }
 
@@ -48,24 +61,31 @@ readonly class SWApi
      */
     public function getFilms(FindFilmsRequest $request): Films
     {
-        $url = $this->base_url.\App\Enums\SWApi::FILMS->value;
-        $url_parameters = [];
-        if (!empty($request->title)){
-            $url_parameters['title'] = $request->title;
+        $url = $this->base_url . \App\Enums\SWApi::FILMS->value;
+        $query_parameters = [];
+        $key = $this->base_key_cache . $url;
+        if (!empty($request->title)) {
+            $query_parameters['title'] = $request->title;
+            $key .= $request->title;
         }
-        $response = Http::withQueryParameters($url_parameters)->get($url);
-        $data = $response->json();
+        $data = Cache::remember(md5($key), $this->expiration_time, function () use ($url, $query_parameters) {
+            $response = Http::withQueryParameters($query_parameters)->get($url);
+            return $response->json();
+        });
         return Films::fromSWApiResponse($data);
     }
 
     public function getFilmsById(string $id): Films
     {
-        $url = $this->base_url.\App\Enums\SWApi::FILMS->value;
-        if (!empty($id)){
-            $url .= '/'.$id;
+        $url = $this->base_url . \App\Enums\SWApi::FILMS->value;
+        if (!empty($id)) {
+            $url .= '/' . $id;
         }
-        $response = Http::get($url);
-        $data = $response->json();
+        $key = $this->base_key_cache . $url;
+        $data = Cache::remember(md5($key), $this->expiration_time, function () use ($url) {
+            $response = Http::get($url);
+            return $response->json();
+        });
         return Films::fromSWApiResponse($data);
     }
 
